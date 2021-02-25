4a3455d74385b9d027601d98f9ca1c9258366951
Ali-AQA <aliabdullaevv@gmail.com>
2021-02-25 15:15:00 +0200

    commmit

--- venv/delete_contact.py
+++ venv/delete_contact.py
@@ -5,6 +5,10 @@ import random
 import string
 from random import randint
 
+url = "http://givvy-api.projestic.com/api/v1/contacts"
+headers = {"Authorization": "Bearer 3cc828bd49e868908b7d5892c3c38f5e16826ab9a5a724aeabd0cd96977784c7",
+           "Content-Type": "application/json", "Accept": "application/json"}
+
 def random_with_N_digits(n):
     range_start = 10 ** (n - 1)
     range_end = (10 ** n) - 1
@@ -17,9 +21,6 @@ def create_contact():
 
     phone = '+38{}'.format(random_with_N_digits(7))
     newEmail = random_char(7) + "@gmail.com"
-    url = "http://givvy-api.projestic.com/api/v1/contacts"
-    headers = {"Authorization": "Bearer 3cc828bd49e868908b7d5892c3c38f5e16826ab9a5a724aeabd0cd96977784c7",
-               "Content-Type": "application/json", "Accept": "application/json"}
     data_dict = {'first_name': 'Test', 'last_name': 'Tester', 'email': newEmail,
                  'mobile_phone': phone}
     create_contact_response = requests.post(url, json=(data_dict), headers=headers)
@@ -27,28 +28,23 @@ def create_contact():
     body = create_contact_response.json()
     assert create_contact_response.status_code == 201
     user_id = body['data']['id']
-    delete_contact(user_id)
-    view_contact(user_id)
+    return user_id
 
 def delete_contact(user_id):
 
-    url = "http://givvy-api.projestic.com/api/v1/contacts/"
-    headers = {"Authorization": "Bearer 3cc828bd49e868908b7d5892c3c38f5e16826ab9a5a724aeabd0cd96977784c7",
-               "Content-Type": "application/json", "Accept": "application/json"}
-    delete_contact_response = requests.delete(url+str(user_id), headers=headers)
+    delete_contact_response = requests.delete(url + '/' + str(user_id), headers=headers)
     print(delete_contact_response.json())
     print(delete_contact_response.status_code)
     assert delete_contact_response.status_code == 200
 
 def view_contact(user_id):
 
-    url = "http://givvy-api.projestic.com/api/v1/contacts/"
-    headers = {"Authorization": "Bearer 3cc828bd49e868908b7d5892c3c38f5e16826ab9a5a724aeabd0cd96977784c7",
-               "Content-Type": "application/json", "Accept": "application/json"}
-    view_contact_response = requests.get(url + str(user_id), headers=headers)
+    view_contact_response = requests.get(url + '/' + str(user_id), headers=headers)
     print(view_contact_response.status_code)
     assert view_contact_response.status_code == 404
 
-create_contact()
+user_id = create_contact()
+delete_contact(user_id)
+view_contact(user_id)
 
 
